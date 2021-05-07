b0ecf710830aa7eb11757e154313b359363b95ba
Abraham Guerrero Godoy <abrahamgg@outlook.com>
2021-05-07 16:36:15 -0500

    Creacion del proyecto

---
+++ src/main/java/com/pima/creditoparati/CreditoParaTiApplication.java
@@ -0,0 +1,13 @@
+package com.pima.creditoparati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditoParaTiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditoParaTiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pima/creditoparati/CreditoParaTiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pima.creditoparati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditoParaTiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
